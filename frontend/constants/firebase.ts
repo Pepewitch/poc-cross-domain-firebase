c309e9207e34bffd222d50f7dae17e3000fd829e
Witchayut <skpepe134@gmail.com>
2022-08-05 00:20:53 +0700

    implement signin with firebase

---
+++ frontend/constants/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWce4Catb6RvxNoM7OqlBYokQutnhSH4o",
+  authDomain: "poc-cross-domain-firebase.firebaseapp.com",
+  projectId: "poc-cross-domain-firebase",
+  storageBucket: "poc-cross-domain-firebase.appspot.com",
+  messagingSenderId: "485336828550",
+  appId: "1:485336828550:web:758171cf2394888d4c9f07",
+  measurementId: "G-BQB27Z7QHL",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,155 @@
-export default function Home() {
+import {
+  signInWithEmailAndPassword,
+  User,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "../constants/firebase";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const signup = async () => {
+    setLoading(true);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      setEmail("");
+      setPassword("");
+      alert(`Register ${email} successful!`);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <form
+      className="flex p-2 flex-col"
+      onSubmit={(e) => {
+        e.preventDefault();
+        signup();
+      }}
+    >
+      <h1 className="mb-2 font-bold">Register new user</h1>
+      <input
+        className="p-1 border mb-2 rounded-md"
+        disabled={loading}
+        value={email}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="p-1 border mb-2 rounded-md"
+        disabled={loading}
+        value={password}
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        disabled={loading}
+        type="submit"
+        className="p-1 border text-white bg-blue-700 rounded-md"
+      >
+        {loading ? "Registering..." : "Register"}
+      </button>
+    </form>
+  );
+};
+
+const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const signin = async () => {
+    setLoading(true);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setEmail("");
+      setPassword("");
+      alert(`Sign in as ${email} successful!`);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <form
+      className="flex p-2 flex-col"
+      onSubmit={(e) => {
+        e.preventDefault();
+        signin();
+      }}
+    >
+      <h1 className="mb-2 font-bold">Sign in</h1>
+      <input
+        className="p-1 border mb-2 rounded-md"
+        disabled={loading}
+        value={email}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="p-1 border mb-2 rounded-md"
+        disabled={loading}
+        value={password}
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        disabled={loading}
+        type="submit"
+        className="p-1 border text-white bg-blue-700 rounded-md"
+      >
+        {loading ? "Signing in..." : "Sign in"}
+      </button>
+    </form>
+  );
+};
+
+export default function Home() {
+  const [currentUser, setCurrentUser] = useState<User>(null);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+  }, []);
+  return (
+    <div className="flex p-2 flex-col items-center max-w-4xl mx-auto">
+      <h1 className="text-3xl font-bold underline mb-6">
+        PoC Cross domain Firebase!
+      </h1>
+      <p className="mb-4 text-xl">
+        {currentUser ? (
+          <>
+            You logged in with{" "}
+            <span className="text-red-700 font-bold underline">
+              {currentUser.email}
+            </span>
+          </>
+        ) : (
+          "You have not logged in"
+        )}
+      </p>
+      {!!currentUser && (
+        <button
+          className="bg-red-700 text-white rounded-md p-2 w-48 mb-6"
+          onClick={() => auth.signOut()}
+        >
+          Logout
+        </button>
+      )}
+      <div className="grid w-full grid-cols-2">
+        <div >
+          <Signup />
+        </div>
+        <div >
+          <Signin />
+        </div>
+      </div>
     </div>
   );
 }
