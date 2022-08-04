7e7743e6dbfa65474393d66bbccff95b90a225ab
Witchayut <skpepe134@gmail.com>
2022-08-05 02:13:42 +0700

    implement session

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,31 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const login = functions.https.onRequest(async (request, response) => {
+  if (request.method !== "POST") {
+    response.status(400).send("Please send a POST request");
+    return;
+  }
+  const expiresIn = 60 * 60 * 24 * 365 * 1000; // set for a year
+  const sessionCookie = await admin
+    .auth()
+    .createSessionCookie(request.body.idToken, { expiresIn });
+  response.cookie("__session", sessionCookie, {
+    maxAge: expiresIn,
+    httpOnly: true,
+    secure: true,
+    domain: '.vercel.app'
+  });
+  response.send({ success: true });
+});
+
+export const status = functions.https.onRequest(async (request, response) => {
+  const sessionCookie: string = request.cookies.__session || "";
+  const decodedIdToken = await admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true);
+  const customToken = await admin.auth().createCustomToken(decodedIdToken.uid);
+  response.send({ customToken });
+});
